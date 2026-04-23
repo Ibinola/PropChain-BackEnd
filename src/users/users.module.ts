e25936fad5adc12fcb45cedd4cb2467f42c41952
Abdulmalik A. (Personal) <amalikabdulmalik04@gmail.com>
2026-04-23 11:30:41 +0100

    chore: apply linting fixes
    - Format code according to ESLint rules
    - Fix line endings and formatting
    
    Issues: #311, #310

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,39 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserPreferencesService } from './user-preferences.service';
 import { UserPreferencesController } from './user-preferences.controller';
 import { ActivityLogService } from './activity-log.service';
 import { ActivityLogController, AdminActivityLogController } from './activity-log.controller';
-import { AvatarUploadController } from './avatar-upload.controller';
-import { AvatarUploadService } from './avatar-upload.service';
-import { ScheduledDeletionService } from './scheduled-deletion.service';
-import { UserImportController } from './user-import.controller';
-import { UserImportService } from './user-import.service';
 import { PrismaModule } from '../database/prisma.module';
-import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ScheduleModule.forRoot()],
+  imports: [PrismaModule],
   controllers: [
     UsersController,
-    AvatarUploadController, 
-    UserImportController,
     UserPreferencesController,
     ActivityLogController,
     AdminActivityLogController,
   ],
-  providers: [
-    UsersService,
-    AvatarUploadService, 
-    ScheduledDeletionService, 
-    UserImportService,
-    UserPreferencesService,
-    ActivityLogService,
-    UsersController,
-    AvatarUploadController
-  ],
-  exports: [UsersService, AvatarUploadService, ScheduledDeletionService, UserImportService, UserPreferencesService, ActivityLogService, AvatarUploadService, ScheduledDeletionService],
+  providers: [UsersService, UserPreferencesService, ActivityLogService],
+  exports: [UsersService, UserPreferencesService, ActivityLogService],
 })
 export class UsersModule {}
